e53b2c33a1b112ef60307c6d75cfc5e77d3e7e65
Alex Davidson <alex.davidson@bluewire-technologies.com>
2017-05-31 20:22:14 +0100

    Implement System performance counter logging

---
+++ Bluewire.Metrics.Service/Configuration/PerformanceCounterMetricsConfigurationElement.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace Bluewire.Metrics.Service.Configuration
+{
+    public class PerformanceCounterMetricsConfigurationElement : ConfigurationElement
+    {
+        [ConfigurationProperty("system", DefaultValue = true)]
+        public bool System => (bool)base["system"];
+    }
+}
---
+++ Bluewire.Metrics.Service/Configuration/SourcesConfigurationElement.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace Bluewire.Metrics.Service.Configuration
+{
+    public class SourcesConfigurationElement : ConfigurationElement
+    {
+        [ConfigurationProperty("performanceCounters")]
+        public PerformanceCounterMetricsConfigurationElement PerformanceCounters => (PerformanceCounterMetricsConfigurationElement)base["performanceCounters"] ?? new PerformanceCounterMetricsConfigurationElement();
+    }
+}
